35b1693e9b75636167e2bbd6bf91edabb226d1f5
AishSI <aishsi@gmail.com>
2023-12-27 18:25:21 +0700

    feat: add getIdentityMatrix solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -465,8 +465,15 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const arr = Array(n).fill(Array(n).fill(0));
+  const res = arr.map((item, index) => {
+    const curr = [].concat(...item);
+    curr[index] = 1;
+    return curr;
+  });
+
+  return res;
 }
 
 /**
